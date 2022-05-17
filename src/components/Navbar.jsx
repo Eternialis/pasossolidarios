752816d8a53fddce9a3141c361d59939a9d1e407
ManuelNelson7 <manuel.nelson7@hotmail.com>
2022-05-17 17:18:17 -0300

    hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,124 @@
 import { Link } from "react-router-dom"
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+    BookmarkAltIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    CursorClickIcon,
+    MenuIcon,
+    PhoneIcon,
+    PlayIcon,
+    RefreshIcon,
+    ShieldCheckIcon,
+    SupportIcon,
+    ViewGridIcon,
+    XIcon,
+} from '@heroicons/react/outline'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Pasos Solidarios</h1>
-            <ul>
-                <li>
-                    <Link to="/">INICIO</Link>
-                </li>
-                <li>
-                    <Link to="/nosotros">NOSOTROS</Link>
-                </li>
-                <li>
-                    <Link to="/sumate">SUMATE</Link>
-                </li>
-                <li>
-                    <Link to="/dona">DONA</Link>
-                </li>
-                <li>
-                    <Link to="/login">LOGIN</Link>
-                </li>
-            </ul>
-        </nav>
+        <Popover className="relative bg-white shadow">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
+                    <div className="flex justify-start lg:w-0 lg:flex-1">
+                        <Link to="/">
+                            <span className="sr-only">Workflow</span>
+                            <img
+                                className="h-8 w-auto sm:h-10"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                alt=""
+                            />
+                        </Link>
+                    </div>
+                    <div className="-mr-2 -my-2 md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open menu</span>
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                    </div>
+                    <Popover.Group as="nav" className="hidden md:flex space-x-10">
+
+                        <Link to="/nosotros" className="text-base font-medium text-gray-600 hover:text-gray-900">
+                            Sobre nosotros
+                        </Link>
+                        <Link to="/noticias" className="text-base font-medium text-gray-600 hover:text-gray-900">
+                            Noticias
+                        </Link>
+                        <Link to="/noticias" className="text-base font-medium text-gray-600 hover:text-gray-900">
+                            Colaborar
+                        </Link>
+                    </Popover.Group>
+                    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                        <a
+                            href="#"
+                            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
+                        >
+                            Sumate
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel
+                    focus
+                    className="absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden"
+                >
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                        <div className="pt-5 pb-6 px-5">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        alt="Workflow"
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">Close menu</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-6 px-5 space-y-6">
+                            <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                                <Link to="/nosotros" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                                    Nosotros
+                                </Link>
+
+                                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                                    Docs
+                                </a>
+                            </div>
+                            <div>
+                                <a
+                                    href="#"
+                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                >
+                                    Sumate
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+        </Popover>
     )
 }
 
